f2c498968f0f0e576bed8d0fd466a2eca63137ed
alessiodelorenzis <alessiodelo01@gmail.com>
2019-06-11 13:55:01 +0200

    ticket #3

--- src/FrontHand/ButtleschipPanel.java
+++ src/FrontHand/ButtleschipPanel.java
@@ -1,5 +1,25 @@
 package FrontHand;
+import java.awt.*;
+import javax.swing.*;
+public class ButtleschipPanel extends JFrame{
 
-public class ButtleschipPanel {
-    // Todo Buttleship
+    private JMenuBar menuBar;
+    private JMenu menu;
+    private JMenuItem Nuovapartita, esci;
+
+
+    public ButtleschipPanel () {
+        setTitle("ButtleShip");
+        setBounds(100, 100, 660, 403);​
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        menuBar = new JMenuBar();
+        menu = new JMenu("File");
+        Nuovapartita = new JMenuItem("Nuova Prtita");
+        esci = new JMenuItem("Esci");
+        menu.add(Nuovapartita);
+        menu.add(esci);
+        contentPane.add(menuBar,BorderLayout.NORTH);
+
+    }
+
 }
