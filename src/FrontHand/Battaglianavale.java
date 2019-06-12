22562525b195f906c3c08c039d549f5b288d4d1f
alessiodelorenzis <alessiodelo01@gmail.com>
2019-06-12 15:41:36 +0200

    ticket #7

--- src/FrontHand/Battaglianavale.java
+++ src/FrontHand/Battaglianavale.java
@@ -1,11 +1,13 @@
-/*package FrontHand;
+
+import sun.misc.JarIndex;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-import FrontEnd.PannelloUtente;
-import BackEnd.GestioneTabella;
+//import FrontEnd.PannelloUtente;
+//import BackEnd.GestioneTabella;
 
 public class Battaglianavale extends JFrame{
 
@@ -23,19 +25,24 @@ public class Battaglianavale extends JFrame{
         });
     }
 
+    private String nomeUtente;
     private JMenuBar menuBar;
     private JMenu menu;
     private JMenuItem Nuovapartita, esci;
     private JPanel contentPane, pannelloComputer, pannelloGiocatore;
     private JTable tablellaComputer, tabellaGiocatore;
+    private JLabel giocatoreX, giocatoreY;
     private JLabel giocatore;
+    private JLabel coordinateGiocatore;
+    private JButton bottoneFuoco;
+    private JTextField giocatoreCoordinataX, giocatoreCoordinataY;
 
     public Battaglianavale () {
         setTitle("BattleShip");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(100,100,500,500);
         contentPane = new JPanel();
-        contentPane.setBorder(new EmptyBorder(5,5,5,5));
+        contentPane.setBorder(new EmptyBorder(5,5,100,100));
         contentPane.setLayout(new BorderLayout(0,0));
         setContentPane(contentPane);
 
@@ -45,14 +52,23 @@ public class Battaglianavale extends JFrame{
         menu = new JMenu("File");
         menuBar.add(menu);
 
+        JFrame questaFinestra = this;
         Nuovapartita = new JMenuItem("Nuova Prtita");
         Nuovapartita.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                PannelloUtente finestraUtente = new PannelloUtente();
-                finestraUtente.setVisible(true);
-                //pannelloComputer.setVisible(true);
-                //pannelloGiocatore.setVisible(true);
+                //PannelloUtente finestraUtente = new PannelloUtente();
+                //finestraUtente.setVisible(true);
+                nomeUtente=JOptionPane.showInputDialog(questaFinestra,"Inserisci Nome");
+                System.out.println(nomeUtente);
+                giocatore.setText(nomeUtente);
+                giocatore.setVisible(true);
+                bottoneFuoco.setVisible(true);
+                giocatoreCoordinataX.setVisible(true);
+                giocatoreCoordinataY.setVisible(true);
+                giocatoreX.setVisible(true);
+                giocatoreY.setVisible(true);
+                coordinateGiocatore.setVisible(true);
                 System.out.println("si funziona!");
 
             }
@@ -65,36 +81,74 @@ public class Battaglianavale extends JFrame{
             public void actionPerformed(ActionEvent e) {
                 //pannelloComputer.setVisible(false);
                 //pannelloGiocatore.setVisible(false);
+                giocatore.setVisible(false);
+                bottoneFuoco.setVisible(false);
+                giocatoreCoordinataX.setVisible(false);
+                giocatoreCoordinataY.setVisible(false);
+                giocatoreX.setVisible(false);
+                giocatoreY.setVisible(false);
+                coordinateGiocatore.setVisible(false);
             }
         });
         menu.add(esci);
 
         pannelloGiocatore = new JPanel();
-        pannelloGiocatore.setVisible(false);
         contentPane.add(pannelloGiocatore,BorderLayout.WEST);
+        contentPane.setBounds(200,200,4000,4000);
 
-        giocatore = new Jlabel();
+        giocatore = new JLabel();
         giocatore.setEnabled(false);
-        giocatore.setBackground(null);
-        giocatore.setText(PannelloUtente.getNuomeUtente());
-        pannelloGiocatore.add(giocatore);
+        //giocatore.setBackground(null);
+        //giocatore.setText(nomeUtente);
+        giocatore.setBounds(20,20, 50,20);
+        pannelloGiocatore.add(giocatore, BorderLayout.NORTH);
 
-        tabellaGiocatore = GestioneTablella.inizializzazioneTabella();
-        pannelloGiocatore.add(tabellaGiocatore);
+        coordinateGiocatore = new JLabel("Coordinate       ");
+        coordinateGiocatore.setVisible(false);
+        coordinateGiocatore.setBounds(10,80,10,10);
+        pannelloGiocatore.add(coordinateGiocatore);
+
+
+        giocatoreX = new JLabel("X");
+        giocatoreX.setBounds(10,40,10,10);
+        pannelloGiocatore.add(giocatoreX);
+        giocatoreX.setVisible(false);
+
+        giocatoreCoordinataX = new JTextField();
+        giocatoreCoordinataX.setBounds(10,40,10,10);
+        pannelloGiocatore.add(giocatoreCoordinataX);
+        giocatoreCoordinataX.setVisible(false);
+        giocatoreCoordinataX.setColumns(2);
+
+        giocatoreY = new JLabel("Y");
+        giocatoreY.setBounds(10,40,10,10);
+        pannelloGiocatore.add(giocatoreY);
+        giocatoreY.setVisible(false);
+
+        giocatoreCoordinataY = new JTextField();
+        giocatoreCoordinataY.setBounds(10,40,10,10);
+        pannelloGiocatore.add(giocatoreCoordinataY);
+        giocatoreCoordinataY.setVisible(false);
+        giocatoreCoordinataY.setColumns(2);
+
+        bottoneFuoco = new JButton("Fire");
+        bottoneFuoco.setBounds(200,50,20,60);
+        pannelloGiocatore.add(bottoneFuoco);
+        bottoneFuoco.setVisible(false);
+        pannelloGiocatore.setBounds(40,40,1000,1000);
+
+
+
+        //tabellaGiocatore = GestioneTablella.inizializzazioneTabella();
+        //pannelloGiocatore.add(tabellaGiocatore);
 
         pannelloComputer = new JPanel();
-        pannelloComputer.setVisible(false);
-        contentPane.add(pannelloComputer,BorderLayout.WEST);
+        contentPane.add(pannelloComputer,BorderLayout.EAST);
 
-        tablellaComputer = GestioneTablella.inizializzazioneTabella();
-        pannelloGiocatore.add(tablellaComputer);
-
-
-
-
-
+        //tablellaComputer = GestioneTablella.inizializzazioneTabella();
+        //pannelloGiocatore.add(tablellaComputer);
 
     }
 
 
-}*/
+}
