0b2c059b0b4b4588f58af1b355f9f59fc56d6a07
MarvinAlexanderPerezAlvarado <alex30122001@hotmail.com>
2019-06-13 14:54:43 +0200

    Ticket #5: Completato

--- src/BackHand/GestioneUtente.java
+++ src/BackHand/GestioneUtente.java
@@ -1,29 +1,46 @@
 package BackHand;
 public class GestioneUtente {
      private String ListY="LIHGFEDCBA";
-     private String [][]A;
-     private String B[][];
-    public GestioneUtente(String [][]p,String[][]l){
-           
+     private String listY="lihgfedcba";
+     private String [][]MatriceB;      //Matrice contenenti navi
+     private String [][]MatriceA;      //Matrice che gestisce gli attacchi
+    public GestioneUtente(String [][]p){
+    MatriceB=p;
+
 
     }
-            public boolean ControlloX(int X){
-                if(X>=10&&X<=0){
-                    return false;
+            public boolean ControlloX(int X){          //ok
+                if(X<10&&X>-1){
+                    return true;
                 }
-                else return true;
+                else return false;
             }
-            public boolean ControlloY(String Y){
-                  boolean YCorrect=ListY.equals(Y);
-                  if(YCorrect==true){
-                      return true;
-                  } else return false;
+            public boolean ControlloY(String Y){         //ok
+                  int YCorrect=ListY.indexOf(Y);
+                if(YCorrect<10&&YCorrect>-1) {
+                    return true;
+                }else {
+                    int ycorrect=listY.indexOf(Y);
+                    if(ycorrect<10&&ycorrect>-1)
+                    {
+                        return true;
+                    }else return false;
+                }
             }
-            public int ConvertitoreY(String Y){
+            public int ConvertitoreY(String Y){         //ok
                  int YConvert=ListY.indexOf(Y);
-                 return YConvert;
+                if(YConvert<10&&YConvert>-1){
+                    return YConvert;
                 }
-            public boolean ControlloCoordinate(int x, String Y){
+                else{
+                    int yconvert=listY.indexOf(Y);
+                    if(yconvert<10&&yconvert>-1){
+                        return yconvert;
+                    }
+                }
+                return -1;
+    }
+            public boolean ControlloCoordinate(int x, String Y){  //ok
                 boolean CheckX=this.ControlloX(x);
                 boolean CheckY=this.ControlloY(Y);
                 if(CheckX==true){
@@ -34,36 +51,68 @@ public class GestioneUtente {
                 }
                 else return false;
         }
+ /*          public void  StampA(){
+                for(int i = 0; i < 2; i++){
+                    for(int j = 0; j < 2; j++){
+                        System.out.println(MatriceA[i][j]);
 
+                    }
+                }
+            }
+            public void  StampB(){
+                for(int i = 0; i < 2; i++){
+                    for(int j = 0; j < 2; j++){
+                        System.out.println(MatriceB[i][j]);
 
-            public String attaccoDalComputer(int x,int Y) {
+                }
+            }
+    }*/
+
+            public void RiempimentoMatriceNonVis(){ //Metodo per riempire la Matrice non vista dall'utente
+                for(int i = 0; i < 10; i++){
+                    for(int j = 0; j < 10; j++){
+                        MatriceA[i][j]=" ";
+
+                    }
+                }
+            }
+
+            public String attaccoDalComputer(int x,int Y) { //ok riceve le coordinate e controlla le due matrici
                 String Hit="X";
                 String NoHit="O";
                 String Wrong="XO";
-                if(B[x][Y].equals('X')){
-                        if(A[x][Y].equals(' ')){
-                            A[x][Y].replace(' ','X');
+                String SegnoVuoto=" ";
+                String SegnoX="X";
+               // String SegnoO="O";
+                if(MatriceB[x][Y].equals(SegnoX)){
+                        if(MatriceA[x][Y].equals(SegnoVuoto)){
+                            MatriceA[x][Y]="X";
                             return Hit;
                         }else return Wrong;
-                }else if(A[x][Y].equals(' ')){
-                    A[x][Y].replace(' ','O');
+                }else if(MatriceA[x][Y].equals(SegnoVuoto)){
+                    MatriceA[x][Y]="O";
                     return NoHit;
                 }else return Wrong;
             }
-            public boolean controllaVittoriaGiocatore(){
+            public boolean controllaVittoriaComputer(){                //ok Controlla la matrice non visualizzata e guarda se ci sono 17 X
+        String SimboloX="X";
                 int Tot=0;
-                for(int k=0; k<10; k++){
-                    for(int kk=0; k<10; kk++){
-                        if(A[k][kk].equals('X')){
+                for(int i=0; i<10; i++){
+                    for(int j=0; j<10; j++){
+                        if(MatriceA[i][j].equals(SimboloX)){
                             Tot++;
                             }
                         }
                     }
-                if(Tot==5) {
+                if(Tot==17) {
                     return true;
                 }
                     else return false;
                 }
+    public String getMatrice(){
+        return MatriceA[10][10];
+    }
 
-            }
+
+}
 
--- src/MainClass.java
+++ src/MainClass.java
@@ -11,10 +11,10 @@ public class MainClass {
     public static void main (String[]args){
         //toDo dobbiamo aprire la finestra
         Scanner s=new Scanner(System.in);
-        int columns = 10;
+        int columns = 2;
         int x,y;
         String o;
-        int rows = 10;
+        int rows = 2;
         String[][] newArray = new String[columns][rows];
         newArray[0][0] = "X";
         newArray[0][1] = " ";
@@ -31,9 +31,36 @@ public class MainClass {
         System.out.println("Insert x: ");
         x=s.nextInt();
         System.out.println("Insert y: ");
-        y=s.nextInt();
-        System.out.println("Insert o: ");
         o=s.next();
-        GestioneUtente g=new GestioneUtente();
+        GestioneUtente g=new GestioneUtente(newArray,ArrayA);
+        boolean l=g.ControlloX(x);
+        System.out.println(" X: "+l);
+        boolean t=g.ControlloY(o);
+        System.out.println(" y: "+t);
+        int q=g.ConvertitoreY(o);
+        System.out.println("YConvert: "+q);
+        boolean f=g.ControlloCoordinate(x,o);
+        System.out.println(" Wow: "+f);
+        y=s.nextInt();
+        g.A();
+       /* g.StampA();
+        System.out.println("-------------------------");
+        g.StampB();
+        System.out.println("-------------------------");
+*/
+        String att=null;
+        att=g.attaccoDalComputer(x,y);
+        System.out.println(" "+att);/*
+        System.out.println("-------------------------");
+        g.StampA();
+        System.out.println("-------------------------");
+        g.StampB();*/
+        System.out.println("-------------------------");
+        att=g.attaccoDalComputer(x,y);
+        System.out.println(" "+att);
+        System.out.println("-------------------------");
+        boolean z= g.controllaVittoriaComputer();
+        System.out.println(" " +z);
     }
+
 }
